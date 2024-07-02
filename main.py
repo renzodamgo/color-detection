900b45bd9b173069d5a10c1ed802f7d66ae69558
Renzo Damian <renzo.damian.go@gmail.com>
2024-07-01 21:39:42 -0500

    Fixed frame with bbox definition when bbox is None

--- main.py
+++ main.py
@@ -20,8 +20,9 @@ while True:
         x1, y1, x2, y2 = bbox
 
         frameWithBbox = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
-
-    cv2.imshow("frame", frameWithBbox)
+        cv2.imshow("frame", frameWithBbox)
+    else:
+        cv2.imshow("frame", frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
