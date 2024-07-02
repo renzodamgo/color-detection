baf014b91710dad4a9446dbc3059990b68c7b75e
Renzo Damian <renzo.damian.go@gmail.com>
2024-07-01 21:06:37 -0500

    Add upper limit and lower limit and show mask filter on window

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import cv2
 from PIL import Image
+from util import get_limits
+
 # Info de la webcam
 cap = cv2.VideoCapture(0)
 
-
-# print(frame)
+red = [40, 20, 150]
+blue = [30,20,150]
 
 while True:
-  ret, frame = cap.read()
-  cv2.imshow('frame', frame)
+    ret, frame = cap.read()
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lowerLimit, upperLimit = get_limits(color=red)
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+    mask_ = Image.fromarray(mask)
+    
+    cv2.imshow("frame", mask)
 
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-    break
-
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
